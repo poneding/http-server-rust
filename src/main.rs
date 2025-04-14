9b4fed3d7222b09702ee3a9e9de6f02a6b861e51
poneding <poneding@gmail.com>
2025-04-14 16:25:26 +0800

    refactor: enhance request handling and response generation in TcpListener

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -12,7 +12,13 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+                let buf_reader = BufReader::new(&mut stream);
+                let request_line = buf_reader.lines().next().unwrap().unwrap();
+                let response = match request_line.as_str() {
+                    "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
+                    _ => "HTTP/1.1 404 NOT FOUND\r\n\r\n",
+                };
+                stream.write_all(response.as_bytes()).unwrap()
             }
             Err(e) => {
                 println!("error: {}", e);
